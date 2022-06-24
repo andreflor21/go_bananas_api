2710945a46a5b9a7f27ee2086a5ee0132e06f33e
Andre Flor <andreflor96@hotmail.com>
2022-06-23 23:06:02 -0300

    add bp to  init - views

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 from .users_view import bp_users
+from .addresses_view import bp_addresses
 
 # from .partners_view import bp_partner
 # from .orders_view import bp_orders
@@ -11,5 +12,5 @@ def init_app(app: Flask) -> None:
 
     app.register_blueprint(bp_index)
     app.register_blueprint(bp_users)
-    # app.register_blueprint(bp_partner)
+    app.register_blueprint(bp_addresses)
     # app.register_blueprint(bp_orders)
